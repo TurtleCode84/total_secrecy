5a83d0d4c8d6dab301eaa311b46e942f54a9d57a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-03-02 17:39:48 +0000

    exit

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ for (const file of eventFiles) {
     console.log(await ratelimitTest.status);
     console.log(await ratelimitTest.json());
     console.log(await ratelimitTest.headers.get('retry-after'));
-    execSync('kill 1');
+    exit(1);
     return;
   };
 
