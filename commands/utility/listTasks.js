e65ccbec9688bf0211f2a9a2d6f9da589e9118bc
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-03-02 21:50:56 -0500

    Fix filename, task path, and deny DM permissions

--- commands/utility/listTasks.js
+++ commands/utility/listTasks.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
-const tasks = require('../tasks');
+const tasks = require('../../tasks');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('listtasks')
-    .setDescription('List all possible tasks'),
+    .setDescription('List all possible tasks')
+    .setDMPermission(false),
   async execute(interaction) {
     const msg = tasks.map(task => `- ${task.name}`).join('\n');
     await interaction.reply({ content: msg, ephemeral: true });
