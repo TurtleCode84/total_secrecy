6a98d739e636b6652d52ee505347f20f23d68d68
silicone-fig <github.siliconecb@skiff.com>
2024-03-03 21:38:57 -0500

    Add /sync command

---
+++ commands/utility/sync.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('sync')
+    .setDescription('Sync all commands with Discord')
+    .setDMPermission(false),
+  async execute(interaction) {
+    if (!interaction.member.roles.cache.find(r => r.id === botInfo.adminRole)) {
+      await interaction.reply({content: 'You do not have permission to use this command!', ephemeral: true});
+      return;
+    }
+
+    const commands = await interaction.client.application.commands.fetch();
+    const kool = await interaction.client.application.commands.set(commands);
+    if (kool.size > 0) {
+      await interaction.reply('Commands synced successfully!');
+    } else {
+      await interaction.reply('Commands failed to sync!');
+    }
+  },
+};
