a33c8e947348199591356f2f7f53cf2b368f70fc
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-03-04 04:06:18 +0000

    Admin restrictions, failing

--- commands/utility/round.js
+++ commands/utility/round.js
@@ -11,7 +11,11 @@ module.exports = {
     if (!botInfo.isGame) {
       await interaction.reply({ content: 'There is no round currently running.', ephemeral: true });
       return;
+    } else if (playerInfo.some(p => p.id == interaction.user.id)) {
+      await interaction.reply({ content: 'You cannot view admin info for a round you are playing in!', ephemeral: true });
+      return;
     }
+
     const players = playerInfo.map(player => `- ${player.username} ${player.failed ? ':x:' : ':white_check_mark:'}: ${tasks[player.task].name}`).join('\n');
     await interaction.reply({ content: `**Players:**\n${players}\n**Listeners:** ${handlerInfo.length}`, ephemeral: true });
   },
--- handlers/noLetter.js
+++ handlers/noLetter.js
@@ -5,20 +5,22 @@ module.exports = [
   {
     name: Events.MessageCreate,
     async execute(memberInfo, message) {
-      if (message.author.bot || !message.guild || message.author.id != memberInfo.id) return;
+      if (message.author.bot || !message.guild || message.author.id != memberInfo.id || playerInfo.find(p => p.id == memberInfo.id).failed) return;
   
       if (message.content.includes(tasks[playerInfo.find(p => p.id == memberInfo.id).task].handler.parameter)) {
         message.guild.channels.cache.get(botInfo.announcementChannel).send(`<@${memberInfo.id}> failed their task!`);
+        playerInfo[playerInfo.findIndex(p => p.id == memberInfo.id)].failed = true;
       }
     },
   },
   {
     name: Events.MessageUpdate,
     async execute(memberInfo, oldMessage, message) {
-      if (message.author.bot || !message.guild || message.author.id != memberInfo.id) return;
+      if (message.author.bot || !message.guild || message.author.id != memberInfo.id || playerInfo.find(p => p.id == memberInfo.id).failed) return;
 
       if (message.content.includes(tasks[playerInfo.find(p => p.id == memberInfo.id).task].handler.parameter)) {
         message.guild.channels.cache.get(botInfo.announcementChannel).send(`<@${memberInfo.id}> failed their task!`);
+        playerInfo[playerInfo.findIndex(p => p.id == memberInfo.id)].failed = true;
       }
     },
   },
