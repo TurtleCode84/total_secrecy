3ada1b8cfb3f16684838b4a489f749ba51e24998
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-03-20 00:49:41 +0000

    Partial rollback

--- events/error.js
+++
@@ -1,8 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-  name: Events.Error,
-  async execute(err) {
-    console.log(err);
-  },
-};
--- events/warn.js
+++
@@ -1,8 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-  name: Events.Warn,
-  async execute(warning) {
-    console.log(warning);
-  },
-};
